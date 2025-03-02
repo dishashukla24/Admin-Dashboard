ed0f23307967bf919dd777a278e3367759fb8946
Disha <disha@Dishas-MacBook-Air.local>
2025-03-02 09:26:51 +0530

    admin-dashboard

---
+++ src/components/settings/Profile.jsx
@@ -0,0 +1,27 @@
+import { User } from "lucide-react";
+import SettingSection from "./SettingSection";
+import profilePic from './profile.png';
+
+const Profile = () => {
+	return (
+		<SettingSection icon={User} title={"Profile"}>
+			<div className='flex flex-col sm:flex-row items-center mb-6'>
+				<img
+					src={profilePic}
+					alt='Profile'
+					className='rounded-full w-20 h-20 object-cover mr-4'
+				/>
+
+				<div>
+					<h3 className='text-lg font-semibold text-gray-100'>Disha Shukla</h3>
+					<p className='text-gray-400'>dishashukla0110@gmail.com</p>
+				</div>
+			</div>
+
+			<button className='bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition duration-200 w-full sm:w-auto'>
+				Edit Profile
+			</button>
+		</SettingSection>
+	);
+};
+export default Profile;
